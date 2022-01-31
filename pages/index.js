e8e69e0b40961c5e0430e9052d080614b820ef2d
Rogerio Lima <61389593+RogerioLimaDev@users.noreply.github.com>
2022-01-31 16:28:06 -0300

    esqueci parenthesis

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Link from 'next/link';
 
 
-function Home(){
+function Home()
+{
     return 
-    <div>
+    (<div>
     <h1>Home Teste</h1>
-    <Link href="/sobre"><a>Saiba mais</a></Link>
-    </div>
+        <Link href="/sobre">
+        <a>Saiba mais</a>
+        </Link>
+    </div>)
 }
 
 
