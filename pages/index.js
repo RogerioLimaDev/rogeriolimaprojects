edd81221743b65056638c8c592d0da85bcb7583f
Rogerio Lima <61389593+RogerioLimaDev@users.noreply.github.com>
2022-01-31 16:34:19 -0300

    Refactor do código

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
+
 function Home(){
-    return <div><h1>Home page</h1></div>
+    return (<div>
+    <h1>Home page</h1>
+    <Link href={sobre}>
+        <a>Sobre</a>
+    </Link>
+    </div>)
 }
 
 
