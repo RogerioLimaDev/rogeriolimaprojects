4dac1a48e99e7839824a1a6b73f0ff81dc542572
Rogerio Lima <61389593+RogerioLimaDev@users.noreply.github.com>
2022-01-31 16:15:10 -0300

    revisando código do link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home(){
     return 
-    <div><h1>Home Teste</h1>
-    <a href="/sobre"> Saiba mais</a>
+    <div>
+    <h1>Home Teste</h1>
+    <a href="/sobre">Saiba mais</a>
     </div>
 }
 
