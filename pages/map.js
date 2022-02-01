18d88acb620ca4edd3272ee4003368c9287d5d5c
Rogerio Lima <61389593+RogerioLimaDev@users.noreply.github.com>
2022-02-01 10:48:00 -0300

    Adicionei paragrafo na home page

--- pages/map.js
+++ pages/map.js
@@ -3,16 +3,16 @@ import ReactMapGL from "react-map-gl";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Link from "next/link";
 
-function Home(){
-    return (<><div>
-        <h1>Home page</h1>
-        <Link href="/sobre">
-            <a>Sobre</a>
-        </Link>
-    </div>
-    <div id="map">
-    </div></>)   
-}
+// function Home(){
+//     return (<><div>
+//         <h1>Home page</h1>
+//         <Link href="/sobre">
+//             <a>Sobre</a>
+//         </Link>
+//     </div>
+//     <div id="map">
+//     </div></>)   
+// }
 // const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/greggs.json?access_token=${process.env.MAPBOX_KEY}&bbox=-0.227654%2C51.464102%2C0.060737%2C51.553421&limit=10`;
 // mapboxgl.accessToken = process.env.MAPBOX_KEY;
 
@@ -26,13 +26,19 @@ export default function Map() {
     longitude: -0.1278,
     zoom: 10
   });
-  return <ReactMapGL
+  return (
+  
+  <div>
+  <h1>Tá complicado...</h1>
+  <p>Tentando renderizar um mapa do Map box usando o Next JS</p>
+  <ReactMapGL
     mapStyle="mapbox://styles/mapbox/streets-v11"
     mapboxApiAccessToken = "pk.eyJ1Ijoicm9nZXJpb2xpbWF0aGVkZXYiLCJhIjoiY2t6M2FoaWs2MDFrdzJ2cDQzaHNkYXJwZSJ9._MyQKNc9qDkcccffQ23bkQ"
-    {...viewport}
     onViewportChange={(nextViewport) => setViewport(nextViewport)}
     >
   </ReactMapGL>
+  </div>)
+
   }
 
 
