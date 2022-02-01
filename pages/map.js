3a0b094216a21b5a46ea1f9e93c2c1e535c63dc9
Rogerio Lima <61389593+RogerioLimaDev@users.noreply.github.com>
2022-01-31 21:36:22 -0300

    modifiquei token Mapbox outra vez

--- pages/map.js
+++ pages/map.js
@@ -1,6 +1,6 @@
-import { accessToken } from "mapbox-gl";
 import { useState } from "react";
 import ReactMapGL from "react-map-gl";
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 // const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/greggs.json?access_token=${process.env.MAPBOX_KEY}&bbox=-0.227654%2C51.464102%2C0.060737%2C51.553421&limit=10`;
 
